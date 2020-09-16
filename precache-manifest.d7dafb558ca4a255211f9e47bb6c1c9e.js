a6e2f94c5024c08efe95207616dcbb684abf5e71
hyoseok <ksh262011@naver.com>
2020-09-16 15:37:04 +0900

    Updates

---
+++ precache-manifest.d7dafb558ca4a255211f9e47bb6c1c9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d70e2560b3e96cf8ec9a71a6275585a",
+    "url": "/MovieApp/index.html"
+  },
+  {
+    "revision": "d8090fed02575bed7be3",
+    "url": "/MovieApp/static/css/main.9df05f8c.chunk.css"
+  },
+  {
+    "revision": "260ba03d70a20222093f",
+    "url": "/MovieApp/static/js/2.6208fde9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MovieApp/static/js/2.6208fde9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8090fed02575bed7be3",
+    "url": "/MovieApp/static/js/main.295f70f3.chunk.js"
+  },
+  {
+    "revision": "6f250c0a7f911f8bfa6c",
+    "url": "/MovieApp/static/js/runtime-main.7114d7b8.js"
+  }
+]);
